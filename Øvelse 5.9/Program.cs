9fa84d349649dd91e5f0a0946ad921bd9187405f
Skole <Skole@DESKTOP-87FGBUN>
2024-04-18 08:25:13 +0200

    Done

--- Øvelse 5.9/Program.cs
+++ Øvelse 5.9/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hvilken tabel vil du have?");
+int tabel = Convert.ToInt32(Console.ReadLine());
+
+for (int tal = 1; tal < 11; tal++)
+{
+    Console.WriteLine(tal * tabel);
+}
